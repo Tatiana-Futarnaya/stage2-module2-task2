daf23effb9c76d54325b5819ef8bea3df6453a99
Tatiana-Futarnaya <tannia93@mail.ru>
2023-07-14 12:31:53 +0300

    first commit

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,34 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/user/*")
+public class AuthFilter implements Filter {
+
+    public void init(FilterConfig config){
+        System.out.println("AccessFilter. Method init()");
+    }
+
+    public void destroy() {
+        System.out.println("AccessFilter. Method destroy()");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        HttpServletResponse response= (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession(false);
+
+        if(session== null){
+            response.sendRedirect("/login.jsp");
+        }else{
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
 }
